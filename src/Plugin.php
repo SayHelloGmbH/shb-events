440edde0707f64b6f9101521a5cd24cc8c176cab
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-05-11 17:32:49 +0200

    Register post type

--- src/Plugin.php
+++ src/Plugin.php
@@ -46,6 +46,8 @@ class Plugin
 	 */
 	private function run()
 	{
+		add_action('init', [$this, 'registerPostType']);
+		add_action('init', [$this, 'addCapabilities']);
 		add_action('plugins_loaded', array($this, 'loadPluginTextdomain'));
 	}
 
@@ -55,5 +57,66 @@ class Plugin
 	public function loadPluginTextdomain()
 	{
 		load_plugin_textdomain('shp_events', false, dirname(plugin_basename($this->file)) . '/languages');
+	}
+
+	/**
+	 * Registers the custom post type
+	 * @return void
+	 */
+	public function registerPostType()
+	{
+
+		$args = apply_filters('shp-events__post_type_args', [
+			'can_export' => false,
+			'capabilities'	=> [
+				'read' => 'read_shp_event',
+				'edit_post' => 'edit_shp_event',
+				'read_post' => 'read_shp_event',
+				'delete_post' => 'delete_shp_event',
+				'edit_posts' => 'edit_shp_event',
+				'edit_others_posts' => 'edit_others_shp_event',
+				'publish_posts' => 'publish_shp_event',
+				'read_private_posts' => 'read_private_shp_event',
+				'delete_posts' => 'delete_shp_event',
+				'delete_private_posts' => 'delete_private_shp_event',
+				'delete_published_posts' => 'delete_published_shp_event',
+				'delete_others_posts' => 'delete_others_shp_event',
+				'edit_private_posts' => 'edit_private_shp_event',
+				'edit_published_posts' => 'edit_published_shp_event',
+			],
+			'has_archive' => false,
+			'map_meta_cap' => false,
+			'menu_icon' => 'dashicons-calendar-alt',
+			'public' => false,
+			'show_in_admin_bar' => true,
+			'show_in_nav_menus' => true,
+			'show_in_rest' => true,
+			'show_ui' => true,
+			'menu_position' => 5,
+			'rewrite' => [
+				'slug' => _x('event', 'URL slug for custom post type', 'shp_fb_event')
+			],
+			'supports' => false,
+			'taxonomies' => [],
+			'labels' => [
+				'name' => _x('Events', 'CPT name', 'shp_fb_event'),
+				'singular_name' => _x('Event', 'CPT singular name', 'shp_fb_event'),
+				'add_new' => _x('Termin erstellen', 'CPT add_new', 'shp_fb_event'),
+				'add_new_item' => _x('Neuer Event', 'cpt name', 'shp_fb_event'),
+				'edit_item' => _x('Event bearbeiten', 'cpt name', 'shp_fb_event'),
+				'new_item' => _x('Neuer Event', 'cpt name', 'shp_fb_event'),
+				'view_item' => _x('Event anzeigen', 'cpt name', 'shp_fb_event'),
+				'view_items' => _x('Events anzeigen', 'cpt name', 'shp_fb_event'),
+				'search_items' => _x('Events durchsuchen', 'cpt name', 'shp_fb_event'),
+				'not_found' => _x('Keine Events', 'cpt name', 'shp_fb_event'),
+				'not_found_in_trash' => _x('Keine Events im Papierkorb', 'cpt name', 'shp_fb_event'),
+				'all_items' => _x('Alle Termine', 'cpt name', 'shp_fb_event'),
+				'archives' => _x('Archiv', 'cpt name', 'shp_fb_event'),
+				'attributes' => _x('Attribute', 'cpt name', 'shp_fb_event'),
+				'name_admin_bar' => _x('Event', 'Label for name admin bar', 'shp_fb_event'),
+			]
+		]);
+
+		register_post_type('shp_event', $args);
 	}
 }
